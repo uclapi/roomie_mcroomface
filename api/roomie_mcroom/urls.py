7363fa06f9049c42a184f8791e9f5d483b7701ff
Wilhelm Klopp <wil.klopp@gmail.com>
2016-09-14 22:36:29 +0100

    Update API paths #1

--- api/roomie_mcroom/urls.py
+++ api/roomie_mcroom/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('rooms.urls'))
+    url(r'^api/admin/', admin.site.urls),
+    url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('rooms.urls'))
 ]
