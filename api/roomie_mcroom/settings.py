f6742e3801419469c37f3b172eeb3d905d0e7a35
abdulfaizp <abdulfaizp@gmail.com>
2016-10-23 17:46:13 +0100

    whitenoise middleware

--- api/roomie_mcroom/settings.py
+++ api/roomie_mcroom/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
