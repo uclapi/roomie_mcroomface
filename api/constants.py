2925018202ba111e062684e328ad22a7904fc4f6
Christopher Hammond <christopherhammond13@gmail.com>
2016-11-24 02:37:25 +0000

    Added Shib base constant

--- api/constants.py
+++ api/constants.py
@@ -1,4 +1,5 @@
 ROOT_URL = 'https://enghub.io/api/v1/'
+SHIB_URL = 'https://enghub.io/Shibboleth.sso/'
 
 STREAM_PUBLISH_URL      = ROOT_URL + 'push.publish'
 STREAM_SUBSCRIBE_URL    = ROOT_URL + 'push.subscribe/'
