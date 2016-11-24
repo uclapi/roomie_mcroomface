8338fc3d632f1e8d481077f76d5f3e6a9e877c6a
Christopher Hammond <christopherhammond13@gmail.com>
2016-11-24 00:13:03 +0000

    Added constants file for non-sensitive settings such as URLs

---
+++ api/constants.py
@@ -0,0 +1,5 @@
+ROOT_URL = 'https://enghub.io/api/v1/'
+
+STREAM_PUBLISH_URL      = ROOT_URL + 'push.publish'
+STREAM_SUBSCRIBE_URL    = ROOT_URL + 'push.subscribe/'
+STREAM_SUBSCRIBE_LP_URL = ROOT_URL + 'push.subscribe_longpoll/'
