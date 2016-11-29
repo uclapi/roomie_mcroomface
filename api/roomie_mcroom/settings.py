66a09e588dc031bd298de920565bf42e73ff0e2b
Christopher Hammond <christopherhammond13@gmail.com>
2016-11-29 17:35:46 +0000

    Hopefully banished error 400 for good

--- api/roomie_mcroom/settings.py
+++ api/roomie_mcroom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['enghub.io']
+ALLOWED_HOSTS = ['enghub.io', 'localhost', '127.0.0.1']
 
 
 # Application definition
