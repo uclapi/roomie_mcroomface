f40c745d96e4fc93400983c47941a5f39ec2713b
Jaromir Latal <latal.jaromir@gmail.com>
2017-06-07 21:14:59 +0100

    adding tests for rooms.utils

--- api/rooms/tests.py
+++ api/rooms/tests.py
@@ -1,11 +1,13 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import TestCase
 
 from rooms.models import UserProfile
+from rooms.utils import convertTime, weekOrWeekend
 
 
-# Create your tests here.
 class ResetQuotaTestCase(TestCase):
     def setUp(self):
         self.u1 = User.objects.create(username="aVeryLongRandomTestingString")
@@ -21,3 +23,26 @@ class ResetQuotaTestCase(TestCase):
 
     def tearDown(self):
         self.u1.delete()
+
+
+class UtilsTestCase(TestCase):
+    def test_convert_time(self):
+        for hours in range(0, 24):
+            for minutes in range(0, 60):
+                time_string = "{}:{}".format(hours, minutes)
+                time_object = convertTime(time_string)
+
+                self.assertEqual(time_object.hour, hours)
+                self.assertEqual(time_object.minute, minutes)
+
+    def test_week_or_weekend(self):
+        weekdays = ["26-02-2016", "07-06-2017", "09-10-2018"]
+        weekends = ["04-06-2016", "04-06-2017", "01-04-2018"]
+
+        for weekday in weekdays:
+            dt = datetime.datetime.strptime(weekday, "%d-%m-%Y")
+            self.assertEqual(weekOrWeekend(dt), "week")
+
+        for weekend in weekends:
+            dt = datetime.datetime.strptime(weekend, "%d-%m-%Y")
+            self.assertEqual(weekOrWeekend(dt), "weekend")
