8f451fee581d99c4b3b293e4b1b685e6ad10804a
Jaromir Latal <latal.jaromir@gmail.com>
2017-06-07 21:33:16 +0100

    adding tests for rooms.views.is_time_valid

--- api/rooms/tests.py
+++ api/rooms/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 
 from rooms.models import UserProfile
 from rooms.utils import convertTime, weekOrWeekend
+from rooms.views import is_time_valid
 
 
 class ResetQuotaTestCase(TestCase):
@@ -46,3 +47,42 @@ class UtilsTestCase(TestCase):
         for weekend in weekends:
             dt = datetime.datetime.strptime(weekend, "%d-%m-%Y")
             self.assertEqual(weekOrWeekend(dt), "weekend")
+
+
+class IsTimeValidTestCase(TestCase):
+    def setUp(self):
+        self.date = datetime.date(2017, 6, 7)
+        self.start_time = datetime.time(10, 0)
+        self.end_time = datetime.time(15, 0)
+
+    def test_is_time_valid_end_before_start(self):
+        self.end_time = datetime.time(9, 0)
+
+        result = is_time_valid(self.date, self.start_time, self.end_time)
+        self.assertFalse(result["success"])
+        self.assertEqual(result["error"]["error"], "Invalid times given.")
+
+    def test_is_time_valid_end_after_closing(self):
+        self.end_time = datetime.time(23, 0)
+
+        result = is_time_valid(self.date, self.start_time, self.end_time)
+        self.assertFalse(result["success"])
+        self.assertEqual(
+            result["error"]["error"],
+            "Choose an end time before closing time."
+        )
+
+    def test_is_time_valid_start_before_opening(self):
+        self.start_time = datetime.time(5, 0)
+
+        result = is_time_valid(self.date, self.start_time, self.end_time)
+        self.assertFalse(result["success"])
+        self.assertEqual(
+            result["error"]["error"],
+            "Choose a starting time after opening time."
+        )
+
+    def test_is_time_valid_everything_is_fine(self):
+        result = is_time_valid(self.date, self.start_time, self.end_time)
+        self.assertTrue(result["success"])
+        self.assertIsNone(result.get("error"))
